3617caa274c185fa183d119e9a468abd3f883dd4
GetLo <davidlo3942@gmail.com>
2016-11-11 15:44:14 -0800

    Got driver code working

--- Mick/MickDriverControl.h
+++ Mick/MickDriverControl.h
@@ -1,29 +1,68 @@
 
 /*
-	Drive
+Drive
 
-	Function for user control that is based off tank drive
-	ch3 <- left drive
-	ch2 <- right drive
-	Only assigns if above a DEADZONE
+Function for user control that is based off tank drive
+ch3 <- left drive
+ch2 <- right drive
+Only assigns if above a DEADZONE
 */
 #define DEADZONE 15
 
 void Drive()
 {
-		int rightPower = vexRT[Ch2];
-		int leftPower = vexRT[Ch3];
+	int rightPower = vexRT[Ch2];
+	int leftPower = vexRT[Ch3];
 
 
-		rightPower = (abs(rightPower) > DEADZONE) ? rightPower : 0;
-		leftPower = (abs(leftPower) > DEADZONE) ? leftPower : 0;
+	rightPower = (abs(rightPower) > DEADZONE) ? rightPower : 0;
+	leftPower = (abs(leftPower) > DEADZONE) ? leftPower : 0;
 
-		motor[leftDriveBack] = leftPower;
-		motor[leftDriveCenter] = leftPower;
-		motor[leftDriveFront] = leftPower;
+	motor[leftDriveBack] = leftPower;
+	motor[leftDriveCenter] = leftPower;
+	motor[leftDriveFront] = leftPower;
 
-		motor[rightDriveBack] = rightPower;
-		motor[rightDriveCenter] = rightPower;
-		motor[rightDriveFront] = rightPower;
+	motor[rightDriveBack] = rightPower;
+	motor[rightDriveCenter] = rightPower;
+	motor[rightDriveFront] = rightPower;
 
+}
+
+/*
+Lift controls
+6U to
+6D to lower Lift
+*/
+void Lift()
+{
+	if(vexRT[Btn6U]==1){
+		motor[leftLift] = -127;
+		motor[rightLift] = -127;
+	}
+	else if(vexRT[Btn6D]==1){
+		motor[leftLift] = 127;
+		motor[rightLift] = 127;
+	}
+	else
+	{
+		motor[leftLift] = 0;
+		motor[rightLift] = 0;
+	}
+}
+
+void Roller()
+{
+	if(vexRT[Btn5U]==1){
+	  motor[leftRoller] = 127;
+	  motor[rightRoller] = 127;
+  }
+  else if(vexRT[Btn5D]==1){
+    motor[leftRoller] = -127;
+    motor[rightRoller] = -127;
+  }
+  else
+  {
+  	 motor[leftRoller] = 0;
+     motor[rightRoller] = 0;
+  }
 }
