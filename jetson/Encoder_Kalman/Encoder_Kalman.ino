c8355d86bd3e1df5ab6c757e513aefda8ed42600
Kevin Joyce <kpjoyce1@gmail.com>
2017-01-10 23:35:10 +0000

    Wrapped Encoder and Kalman filter into single class for easier use and multiple encoder

--- jetson/Encoder_Kalman/Encoder_Kalman.ino
+++ jetson/Encoder_Kalman/Encoder_Kalman.ino
@@ -1,32 +1,28 @@
 
 #include <Encoder.h>
 
-Encoder test(2, 3);
-
-//process variance
-#define Q 0.01
-//measurement variance
-#define R 0.001
-
-float xhat;      //a posteri estimate of x
-float P;         //a posteri error estimate
-float xhatMinus; //a priori estimate of x
-float PMinus;    //a priori error estimate
-float K;         //gain of blending factor
-float z;         //measurement
-
-void setup()
+class Encoder_Kalman
 {
-  Serial.begin(9600);
+  public:
+    Encoder enc;
+    //process variance
+    float Q = 0.01;
+    //measurement variance
+    float R = 0.001;
+
+    float xhat;      //a posteri estimate of x
+    float P;         //a posteri error estimate
+    float xhatMinus; //a priori estimate of x
+    float PMinus;    //a priori error estimate
+    float K;         //gain of blending factor
+    float z;         //measurement
   
-  xhat = 0;
-  P = 1;
-}
-
-void loop()
-{
+     
+  /*
+  void Update()
+  {
    //Sensor Reading
-   z = test.read();
+   z = enc.read();
    
    //time update
    xhatMinus = xhat;
@@ -36,8 +32,28 @@ void loop()
    K = PMinus / (PMinus + R);
    xhat = xhatMinus + K * (z - xhatMinus);
    P = (1 - K) * PMinus;
+    
+  }
+  */
+};
+
+Encoder_Kalman::Encoder_Kalman(int A, int B)
+{
+  enc = enc(A, B);
+  xhat = 0;
+  P = 1;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  
+}
+
+void loop()
+{
    
-   Serial.println(xhat);
    delay(25);
 }
 
