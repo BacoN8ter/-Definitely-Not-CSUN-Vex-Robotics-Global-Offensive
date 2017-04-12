972607ac6a55103466861b4d1e590899e5c27152
Kevin <kpjoyce1@gmail.com>
2017-04-12 06:09:30 +0000

    Adding tared quaternion and parsing to Yost

--- jetson/Yost_3_Space/main.cpp
+++ jetson/Yost_3_Space/main.cpp
@@ -21,10 +21,65 @@
 
 #define DEBUG 1
   
+struct Quaternion
+{
+  float a, b, c, w;
+};
+
+Quaternion q;
+
+void ParseQuaternion(char * buf, int bufLen)
+{
+  int index = 0;
+  int count = 0;
+  char number[10];
+  
+  for(int i = 0; i < bufLen; i++)
+  {
+    if(buf[i] != ',' && i != bufLen - 1)
+    {
+      number[index] = buf[i];
+      index++;
+    }
+    else
+    {
+      number[index + 1] = '\0';
+      switch(count)
+      {
+
+      case 0:
+	q.a = atof(number); 
+	printf("A:%f\n",q.a);
+	break;
+      case 1:
+	q.b = atof(number); 
+	printf("B:%f\n",q.b);
+	break;
+      case 2:
+	q.c = atof(number); 
+	printf("C:%f\n",q.c);
+	break;
+      case 3:
+	q.w = atof(number); 
+	printf("W:%f\n",q.w);
+	break;
+      }
+
+      count++;
+      i++;
+      index = 0;
+    }
+  }
+
+}
+
 int main(int argc, char *argv[])
 {
   int fd, n, i;
   const int bufSize = 128;
+
+  char tarCmd [] = {':', 0x60, '0', '\n'};
+  char Quatcmd[] = {':', '0','1' ,'\n'};
   char buf[bufSize];
 
   struct termios toptions;
@@ -51,21 +106,26 @@ int main(int argc, char *argv[])
   /* commit the serial port settings */
   tcsetattr(fd, TCSANOW, &toptions);
 
+  write(fd, &tarCmd[0], 1);
+  write(fd, &tarCmd[1], 1);
+  write(fd, &tarCmd[2], 1);  
+  write(fd, &tarCmd[3], 1);
  
   while(true)
   {
-    char cmd[] = {':', '1','1' ,'\n'};
-    write(fd, &cmd[0], 1);
-    write(fd, &cmd[1], 1);
+
+    write(fd, &Quatcmd[0], 1);
+    write(fd, &Quatcmd[1], 1);
     //write(fd, &cmd[2], 1);  
     write(fd, &cmd[3], 1);
     
     memset(buf, '\0', bufSize);
     n = read(fd, buf, bufSize);
+    system("clear");
     /* insert terminating zero in the string */
+    ParseQuaternion(buf, n);
     buf[n] = 0;
     
-    printf("%i bytes read, buffer contains: \n\%s\n", n, buf);
     usleep(100000);
   }
   return 0;
